c7c5a4b9731cdd25e58f5fc1793dc76c723fe740
Rahul Goti <gotirahul96@gmail.com>
2019-12-11 19:59:05 +0530

    Added graph

--- lib/views/dashboard/Accounts/BTC_GBP_Graph.dart
+++ lib/views/dashboard/Accounts/BTC_GBP_Graph.dart
@@ -2,6 +2,8 @@
 import 'package:fluencybank/components/raised_gradient_button.dart';
 import 'package:fluencybank/views/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
 class BTC_GBP_Graph extends StatefulWidget {
   BTC_GBP_Graph({Key key}) : super(key: key);
 
@@ -10,8 +12,51 @@ class BTC_GBP_Graph extends StatefulWidget {
 }
 
 class _BTC_GBP_GraphState extends State<BTC_GBP_Graph> {
+
+ 
+  List<charts.Series> seriesList;
+  bool animate;
+  List<String> dayslist = ["1D","3D","1W","1M","3M","6M","1Y"];
+
+  
+  static List<charts.Series<LinearBitcoinRataes, DateTime>> _createSampleData() {
+    final data = [
+      new LinearBitcoinRataes(6000, DateTime(2017,9,9)),
+      new LinearBitcoinRataes(6001, DateTime(2017,9,10)),
+      new LinearBitcoinRataes(5002, DateTime(2017,9,20)),
+      new LinearBitcoinRataes(4003, DateTime(2017,9,21)),
+      new LinearBitcoinRataes(8004, DateTime(2017,9,22)),
+      new LinearBitcoinRataes(4058, DateTime(2017,9,28)),
+      new LinearBitcoinRataes(5025, DateTime(2017,9,31)),
+      new LinearBitcoinRataes(1065, DateTime(2017,11,25)),    
+    ];
+
+    return [
+      new charts.Series<LinearBitcoinRataes, DateTime>(
+        id: 'Sales',
+        colorFn: (_, __) => charts.ColorUtil.fromDartColor(Color.fromRGBO(0, 179, 223, 1)),
+        domainFn: (LinearBitcoinRataes sales, _) => sales.day,
+        measureFn: (LinearBitcoinRataes sales, _) => sales.rate,
+        data: data,
+      )
+    ];
+  }
+  @override
+  void initState() { 
+    super.initState();
+    _createSampleData();
+  }
+  
+  
+  
   @override
   Widget build(BuildContext context) {
+   
+  
+    setState(() {
+      seriesList = _createSampleData();
+    });
+  
     return Scaffold(
         appBar: AppBar(
           centerTitle: true,
@@ -148,15 +193,60 @@ class _BTC_GBP_GraphState extends State<BTC_GBP_Graph> {
                                       ),
                                     ),
                                   ),
-                                  // //test 
-                                  // Padding(
-                                  //   padding: EdgeInsets.only(top :10),
-                                  //   child: Container(
-                                  //     child: Row(
-                                  //       mai
-                                  //     ),
-                                  //   ),
-                                  //)
+                                  //test 
+                                  Padding(
+                                    padding: EdgeInsets.only(top :10),
+                                    child: Container(
+                                        height: 450,
+                                        child: 
+                                        new charts.TimeSeriesChart(seriesList,
+                                        animate: true,
+                                        animationDuration: Duration(milliseconds: 500),
+                                        //defaultRenderer: new charts.LineRendererConfig(includePoints: true)
+                                        behaviors: [
+                                           new charts.ChartTitle('Days',
+                                          behaviorPosition: charts.BehaviorPosition.bottom,
+                                          titleOutsideJustification:
+                                          charts.OutsideJustification.middleDrawArea),
+                                          new charts.ChartTitle('Currency in £',
+                                          behaviorPosition: charts.BehaviorPosition.start,
+                                          titleOutsideJustification:
+                                          charts.OutsideJustification.middleDrawArea)
+                                        ],
+                                        dateTimeFactory: const charts.LocalDateTimeFactory(),
+                                        ),
+                                      ),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(top : 35),
+                                    child: Container(
+                                      height: 45,
+                                      child: ListView.builder(
+                                        scrollDirection: Axis.horizontal,
+                                        shrinkWrap: true,
+                                        itemCount: dayslist.length,
+                                        itemBuilder: (BuildContext context , int index){
+                                          return GestureDetector(
+                                            onTap: (){},
+                                             child: Padding(
+                                               padding: const EdgeInsets.only(left : 15 , right: 15),
+                                               child: Container(
+                                                height: 50,
+                                                width: 50,
+                                                decoration: BoxDecoration(
+                                                  borderRadius: BorderRadius.circular(10),
+                                                  color: Colors.black
+                                                ),
+                                                child: Center(
+                                                  child: Text(dayslist[index],style:AppStyles.font20.copyWith(color: Colors.white)),
+                                                ),
+                                            ),
+                                             ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  )
                                   ]
                                   )
                                   )
@@ -169,3 +259,14 @@ class _BTC_GBP_GraphState extends State<BTC_GBP_Graph> {
                                   );
   }
 }
+
+
+//Graph 
+
+class LinearBitcoinRataes {
+  final DateTime day;
+  final int rate;
+  LinearBitcoinRataes(this.rate,this.day);
+}
+
+
